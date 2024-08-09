00180ce532157c2be79c1bb5de1cb8807e6fa104
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-09 12:08:38 -0300

    create TodoForm component

---
+++ src/components/TodoForm.js
@@ -0,0 +1,25 @@
+import react, { useState } from 'react'
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!value) return
+    addTodo(value)
+    setValue('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder='Add new todo'
+      />
+    </form>
+  )
+}
+
+export default TodoForm
