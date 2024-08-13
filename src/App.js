e94963cc256def386f916dc4178243afa474daf7
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-13 16:32:04 -0300

    add components to the App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import React, { useState } from 'react'
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
+
 function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Learn React', isCompleted: false },
+    { text: 'Do stuff', isCompleted: true },
+    { text: 'be ultra', isCompleted: false },
+  ])
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true
+    setTodos(newTodos)
+  }
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
   return (
-    <div className='App'>
-      <h1>Todo List App</h1>
+    <div className='app'>
+      <h1>Todo List</h1>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   )
 }
